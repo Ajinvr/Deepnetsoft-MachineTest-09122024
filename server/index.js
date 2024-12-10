c78eb1f3aa78500080c25db97e08b7dc7896f2bd
Ajinvr <ajinvr44@gmail.com>
2024-12-10 11:10:46 +0530

    config update

--- server/index.js
+++ server/index.js
@@ -1,4 +1,24 @@
-import app from './app.js';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import connectDB from './db/config/mongodbconfig.js';
+import menuroutes from './routes/menuRoutes.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors('*'));
+app.use(bodyParser.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({ message: 'server is up' });
+});
+
+app.use('/menu', menuroutes);
 
 const PORT = process.env.PORT || 5000;
 
